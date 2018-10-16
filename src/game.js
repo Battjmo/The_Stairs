4441170cf5a5cca94252504c96a3c65f3d2b5337
Nick Battjes <nhbattjes@gmail.com>
2018-10-15 17:04:23 -0700

    actually works now

---
+++ src/game.js
@@ -0,0 +1,11 @@
+class Game {
+    constructor(context) {
+        this.context = context;
+        this.canvasHeight = 700;
+        this.canvasWidth = 900;
+    }
+}
+
+
+
+export default Game;
--- src/theStairs.js
+++ src/theStairs.js
@@ -1,3 +1,10 @@
+import Game from './game';
+
 document.addEventListener("DOMContentLoaded", () => {
-    
+    const gameCanvas = document.getElementById("game");
+    const context = gameCanvas.getContext("2d");
+    const currentGame = new Game(context);
+    gameCanvas.height = currentGame.canvasHeight;
+    gameCanvas.width = currentGame.canvasWidth;
 });
+
