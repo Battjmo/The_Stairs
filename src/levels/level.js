74c53e6add7ff755260f852d80d9f4d858c04693
Nick Battjes <nhbattjes@gmail.com>
2018-10-29 16:04:21 -0700

    substantial stair improvement

--- src/levels/level.js
+++ src/levels/level.js
@@ -10,7 +10,7 @@ class Level {
         this.pathY = this.pathSize;
         this.pathStart = [0, 0];
         this.shuffle = this.shuffle.bind(this);
-        this.validPath = this.validPath.bind(this);
+        // this.validPath = this.validPath.bind(this);
         this.path = this.pathGenerator();
     }
 
@@ -35,18 +35,19 @@ class Level {
 
     pathGenerator() {
         let path = [this.pathStart];
-        // , 
         let moves = [[0, this.pathSize], [this.pathSize, 0], [-this.pathSize, 0], [0, -this.pathSize]];
-        while (path[path.length - 1][0] + 100 < this.xBound || path[path.length - 1][1] + 100 < this.yBound ||
-            path[path.length - 1][0] <= 0 || path[path.length - 1][1] <= 0) {
+        console.log(path[path.length - 1][0] + 100);
+        while (path[path.length - 1][0] + 100 < this.xBound && path[path.length - 1][1] + 100 < this.yBound) {
             let shuffledMoves = this.shuffle(moves);
-            let currentMove = this.validPath(path, shuffledMoves);
+            let currentMove = this.validMove(path, shuffledMoves);
+            path = path.slice();
             path.push(currentMove);
+            console.log(path);
             }
         return path;
     }
 
-    validPath(path, shuffledMoves) {
+    validMove(path, shuffledMoves) {
         // remove out of bounds 
         let workingMoves = shuffledMoves.slice();
         let currentMoveIndex = 0;
@@ -59,18 +60,16 @@ class Level {
         //check for path collision
         let currentMove = [path[path.length - 1][0] + workingMoves[0][0], path[path.length - 1][1] + workingMoves[0][1]];
         for (let j = 0; j < path.length; j++) {
-                console.log("path: ", path);
-                console.log("currentMove: ", currentMove);
                 if (path[j][0] === currentMove[0] && path[j][1] === currentMove[1]) {
                     console.log("in here");
                     console.log(currentMoveIndex);
                     currentMoveIndex++;
                     console.log(workingMoves[currentMoveIndex]);
                     currentMove = [path[path.length - 1][0] + workingMoves[currentMoveIndex][0], path[path.length - 1][1] + workingMoves[currentMoveIndex][1]];
-                }
             }
         return currentMove;
     }
+}
     //     for (let i = path.length - 4; i < path.length - 1; i++) {
     //         if (path[i]) {
     //             for (let j = 0; j < 2; j++) {
